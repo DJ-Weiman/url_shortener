21b19357efa6a54998b65cfedba8f6816adaf585
Dion Weiman <dionweiman1998@gmail.com>
2025-05-24 18:47:49 +0530

    Added Initial Security Config

--- src/main/java/com/url/shortener/models/ClickEvent.java
+++ src/main/java/com/url/shortener/models/ClickEvent.java
@@ -11,6 +11,7 @@ public class ClickEvent  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private LocalDateTime clickDate;
 
     @ManyToOne
--- src/main/java/com/url/shortener/models/UrlMapping.java
+++ src/main/java/com/url/shortener/models/UrlMapping.java
@@ -12,6 +12,7 @@ public class UrlMapping {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String originalUrl;
     private String shortUrl;
     private int clickCount = 0;
